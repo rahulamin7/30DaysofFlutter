bb1a2e5adab73ce50ac3f574d23374672d2874f9
Rahul Amin <25000watts@gmail.com>
2021-08-08 21:04:46 +0530

    DAy 11 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,12 +1,25 @@
-class Items {
-  final String id;
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "Iphone 12 Pro",
+        desc: "Iphone 12 is 12th generation phone",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://rukminim1.flixcart.com/image/416/416/kg8avm80/mobile/u/q/d/apple-iphone-12-pro-dummyapplefsn-original-imafwgbrcgcar4rn.jpeg?q=70")
+  ];
+}
+
+class Item {
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Items(
+  Item(
       {required this.id,
       required this.name,
       required this.desc,
@@ -14,13 +27,3 @@ class Items {
       required this.color,
       required this.image});
 }
-
-final products = [
-  Items(
-      id: "codepur001",
-      name: "Iphone 12 Pro",
-      desc: "Iphone 12 is 12th generation phone",
-      price: 999,
-      color: "#33505a",
-      image: "https://rukminim1.flixcart.com/image/416/416/kg8avm80/mobile/u/q/d/apple-iphone-12-pro-dummyapplefsn-original-imafwgbrcgcar4rn.jpeg?q=70")
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Rahul";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(4, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.white,
@@ -15,9 +18,15 @@ class HomePage extends StatelessWidget {
             //style: TextStyle(color: Colors.black),
             ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Weocome to $days days of flutter by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
