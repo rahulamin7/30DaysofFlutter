671ffb99a3767f3aca23d5720a0cc6b842672de7
Rahul Amin <25000watts@gmail.com>
2021-08-08 07:21:35 +0530

    day2 Competed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
-        home: Material(
-      child: Center(
-        child: Container(
-          child: Text("Weocome to 30 days of flutter"),
-        ),
-      ),
-    ));
+        home: HomePage());
   }
 }
